fdee5256c8e47c1f86d731b727bbd5d7bd667c2e
Maxime Audouin <coucou747@gmail.com>
2012-03-11 23:55:48 +0100

    compiling script update

--- cmpAll.sh
+++ cmpAll.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo "C"
+echo "\nC"
 gcc $1.c
 cat $2 | ./a.out
-echo "C++"
+echo "\nC++"
 g++ $1.cc
 cat $2 | ./a.out
-echo "java"
+echo "\njava"
 javac $1.java
 cat $2 | java $1
-echo "php"
+echo "\nphp"
 cat $2 | php $1.php
-echo "ocaml"
+echo "\nocaml"
 cat $2 | ocaml $1.ml
