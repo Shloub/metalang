86f0af596b7e293bad975a5a1b7a7091de174371
Maxime Audouin <coucou747@gmail.com>
2012-03-30 12:38:43 +0200

    testing with bytecode compiler : it's faster

--- testAll.sh
+++ testAll.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-ocamlbuild Main/main.native  || exit 1
+ocamlbuild Main/main.byte  || exit 1
 
 for i in `ls tests/prog/*.metalang`; do
     test=`basename "$i" | cut -d . -f 1`
     echo $test
-    ./main.native "$i"  || exit 1
+    ./main.byte "$i"  || exit 1
     bash cmpAll.sh "$test" "tests/prog/$test.in"  || exit 1
     rm "$test.c"
     rm "$test.cc"
