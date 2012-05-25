8a2099e83199f93f4881c966aa2bfa846f973062
Arthur Wendling <art.wendling@gmail.com>
2012-05-25 03:02:52 +0200

    add command line arguments (target languages, output dir, input files)

--- Main/main.ml
+++ Main/main.ml
@@ -20,16 +20,22 @@
 * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*)
+
+(** Main 
 *
-* @see http://prologin.org
-* @author Prologin <info@prologin.org>
-* @author Maxime Audouin <coucou747@gmail.com>
-*
+* @see <http://prologin.org> Prologin
+* @author Prologin (info@prologin.org)
+* @author Maxime Audouin (coucou747@gmail.com)
+* @author Arthur Wendling (art.wendling@gmail.com)
 *)
 
 
 open Stdlib
 open Ast
+open Arg
+
+(** {2 Languages definition } *)
 
 let default_passes (prog : Prog.t) : Prog.t =
      prog
@@ -38,86 +44,125 @@ let default_passes (prog : Prog.t) : Prog.t =
      |> Passes.WalkNopend.apply
      |> Passes.WalkExpandPrint.apply
 
-let tex_passes prog = prog
+let clike_passes prog =
+	prog |> default_passes
+	|> Passes.WalkAllocArrayExpend.apply
+	|> Passes.WalkExpandReadDecl.apply
 
 let ocaml_passes prog =
-  prog |> default_passes |> Passes.WalkIfMerge.apply
+	prog |> default_passes
+	|> Passes.WalkIfMerge.apply
 
-let clike_passes prog =
-    prog |> default_passes |> Passes.WalkAllocArrayExpend.apply
-     |> Passes.WalkExpandReadDecl.apply
-let () =
-  let stdlib_file = "Stdlib/stdlib.metalang" in
-  let filename = Sys.argv.(1) in
-  let progname = Filename.basename filename |> Filename.chop_extension in
-  let () = begin
-    Passes.Rename.add "out"; (* noms à renommer automatiquement *)
-    Passes.Rename.add "exp";
-    Passes.Rename.add "min";
-    Passes.Rename.add "max";
-    Passes.Rename.add progname;
-  end in
-  let out ext printer prog passes =
-    let filename = progname ^ "." ^ ext in
-    Printf.printf "Generating %s\n%!" filename;
-    Fresh.fresh_init prog; (* var names generation init *)
-    let prog = passes prog in
-    let chan = open_out filename in
-    let buf = Format.formatter_of_out_channel chan in
-    let () = Format.fprintf buf "%a@;%!" printer prog in
-    let () = close_out chan in ()
-  in
+let tex_passes prog = prog
+
+module L = MakeMap (String)
+let languages, printers =
+	let ( => ) pa pr out prog = pr out (pa prog) in
+	let ls = [
+		"c",    clike_passes => CPrinter.printer#prog ;
+		"cc",   clike_passes => CppPrinter.printer#prog ;
+		"cs",   clike_passes => CsharpPrinter.printer#prog ;
+		"java", clike_passes => JavaPrinter.printer#prog ;
+		"ml",   ocaml_passes => OcamlPrinter.printer#prog ;
+		"php",  clike_passes => PhpPrinter.printer#prog ;
+		"py",   clike_passes => PyPrinter.printer#prog ;
+		"tex",  tex_passes   =>  TexPrinter.printer#prog ;   
+	] in
+	List.map fst ls, L.from_list ls
+
+let remove_unknown_languages =
+	let go lang =
+		L.mem lang printers
+		|| (Printf.fprintf stderr "unknown language %s\n" lang ; false)
+	in List.filter go
+
+let stdlib_file = "Stdlib/stdlib.metalang"
+
+(** {2 Command Line Arguments} *)
+
+type config = {
+	mutable languages : string list ;
+	mutable output_dir : string ;
+	mutable filenames : string list ;
+}
+
+let default_config () = {
+	languages = languages ;
+	output_dir = "." ;
+	filenames = [] ;
+}
+
+let config () =
+	let c = default_config () in
+	let all = String.concat "," languages in
+	let split ls = remove_unknown_languages $ Str.split (Str.regexp ",") ls in
+	let descr = "metalang [-lang " ^ all ^ "] [-o directory] A.metalang B.metalang ..." in
+	let spec = [
+		"-lang", String (fun ls -> c.languages <- split ls),
+		"List of target languages separated by commas"
+		^ " [default is " ^ all ^ "]" ;
+
+		"-o", String (fun o -> c.output_dir <- o),
+		"Output directory [default is .]" ;
+	] in
+	Arg.parse spec (fun f -> c.filenames <- f :: c.filenames) descr ;
+	c
+
+(** {2 Process} *)
+
+let parse_file parse filename =
   let lexbuf = Lexing.from_channel (open_in filename) in
-  let stdlib_buf = Lexing.from_channel (open_in stdlib_file) in
-  try
-    let (funs, main) = Parser.prog Lexer.token lexbuf in
-    let stdlib_functions = Parser.toplvls Lexer.token stdlib_buf in
-    let prog = {
-      Prog.progname = progname;
-      Prog.funs = funs;
-      Prog.main = main} in    
-    let used_functions = Passes.WalkCollectCalls.fold prog in
-(*    let funs_add = List.filter (* sélection des fonctions de la stdlib *)
-		  (function
-		    | Prog.DeclarFun (v, _,_, _)
-		    | Prog.Macro (v, _, _, _) -> BindingSet.mem v used_functions
-		    | _ -> failwith ("bad stdlib")
-		  )
-		  stdlib_functions
-    in *)
-    let funs_add, _ = List.fold_right
-      (fun f (li, used_functions) -> match f with
+  try parse Lexer.token lexbuf
+	with Parser.Error ->
+    let curr = lexbuf.Lexing.lex_curr_p in
+    let line = curr.Lexing.pos_lnum in
+    let cnum = curr.Lexing.pos_cnum - curr.Lexing.pos_bol - 1 in
+    let tok = Lexing.lexeme lexbuf in
+    Format.printf "%s: error line %d char %d on token %s\n"
+			filename line cnum tok ;
+		exit 1
+
+let make_prog filename =
+	let progname = Filename.chop_extension $ Filename.basename filename in
+	let funs, main = parse_file Parser.prog filename in
+	let stdlib = parse_file Parser.toplvls stdlib_file in
+  let prog = {
+		Prog.progname = progname ;
+		Prog.funs = funs ;
+		Prog.main = main ;
+	} in    
+	let used_functions = Passes.WalkCollectCalls.fold prog in
+	let go f (li, used_functions) = match f with
 	| Prog.DeclarFun (v, _,_, _)
 	| Prog.Macro (v, _, _, _) ->
 	  if BindingSet.mem v used_functions
 	  then (f::li, (Passes.WalkCollectCalls.fold_fun used_functions f) )
 	  else (li, used_functions)
-	| _ -> failwith ("bad stdlib")
-      )
-      stdlib_functions
-      ([], used_functions)
+	| _ -> failwith ("bad stdlib") in
+	let funs_add, _ = List.fold_right go stdlib ([], used_functions) in
+  let funs = funs_add @ funs in
+  { prog with Prog.funs = funs }
 
-    in let funs = funs_add @ funs in
-    let prog = { prog with Prog.funs = funs } in
-    begin
-      out "java" JavaPrinter.printer#prog prog clike_passes;
-      out "c" CPrinter.printer#prog prog clike_passes;
-      out "cc" CppPrinter.printer#prog prog clike_passes;
-      out "ml" OcamlPrinter.printer#prog prog ocaml_passes;
-      out "php" PhpPrinter.printer#prog prog clike_passes;
-      out "py" PyPrinter.printer#prog prog clike_passes;
-      out "cs" CsharpPrinter.printer#prog prog clike_passes;
-      out "tex" TexPrinter.printer#prog prog tex_passes;
-      (* out "sch" SchemePrinter.printer#prog prog clike_passes; *)
-      (* out "sh" BashPrinter.printer#prog prog clike_passes; *)
-    end
-  with Parsing.Parse_error
-	   | Parser.Error ->
-    let curr = lexbuf.Lexing.lex_curr_p in
-    let line = curr.Lexing.pos_lnum in
-    let cnum = curr.Lexing.pos_cnum - curr.Lexing.pos_bol - 1 in
-    let tok = Lexing.lexeme lexbuf in
-    let () = Format.printf "error line %d char %d on token %s\n"
-      line cnum
-      tok
-    in exit 1;;
+let process output_dir languages filename =
+	let prog = make_prog filename in
+	let go lang =
+		let printer = L.find lang printers in
+		let output = output_dir ^ "/" ^ prog.Prog.progname ^ "." ^ lang in
+    Printf.printf "Generating %s\n%!" output ;
+		Fresh.fresh_init prog ;
+		let chan = open_out output in
+    let buf = Format.formatter_of_out_channel chan in
+    Format.fprintf buf "%a@;%!" printer prog ;
+    close_out chan in
+	List.iter go languages
+
+let process_config c =
+	List.iter (process c.output_dir c.languages) c.filenames
+
+let () =
+	(* noms à renommer automatiquement *)
+	Passes.Rename.add "out" ;
+	Passes.Rename.add "exp" ;
+	Passes.Rename.add "min" ;
+	Passes.Rename.add "max" ;
+	process_config $ config ()
