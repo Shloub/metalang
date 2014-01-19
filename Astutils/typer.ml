181d4d3b0e9f369e4a1232a79c93b0210f247bf9
Maxime AUDOUIN <coucou747@gmail.com>
2014-01-19 14:33:04 +0100

    better error messages from Typer

--- Astutils/typer.ml
+++ Astutils/typer.ml
@@ -619,7 +619,8 @@ let rec collect_contraintes_instructions env instructions
                 StringMap.add var ty env.locales }
           in env
         | Instr.StdinSep -> env
-        | Instr.Unquote li -> assert false
+        | Instr.Unquote li ->
+	  raise (Error (fun f -> Format.fprintf f "There is still unquote near %a" ploc loc ))
     ) env instructions
 
 let collect_contraintes e
@@ -789,9 +790,17 @@ let process (prog: 'lex Prog.t) =
   in env, prog
 
 let type_for_enum en env =
-  let (t, _) = StringMap.find en env.enum in
-      t
+  try
+    let (t, _) = StringMap.find en env.enum in
+    t
+  with Not_found _ ->
+    raise ( Error (fun f ->
+      Format.fprintf f "Cannot find type for enum field %s\n" en))
 
 let typename_for_enum en env =
-  let (_, name) = StringMap.find en env.enum in
-      name
+  try
+    let (_, name) = StringMap.find en env.enum in
+    name
+  with Not_found _ ->
+    raise ( Error (fun f ->
+      Format.fprintf f "Cannot find typename for enum field %s\n" en))
