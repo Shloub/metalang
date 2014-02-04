6426a056aaff4648cf49c310e9be27a3ab8eefcb
Maxime AUDOUIN <coucou747@gmail.com>
2014-02-04 20:58:33 +0100

    better php printing

--- out/aaa_readints.php
+++ out/aaa_readints.php
@@ -1,32 +1,8 @@
 <?php
 
-$stdin='';
-function stdin_(){   global $stdin; if ( !feof(STDIN)) $stdin.=fgets(STDIN)."\n";}
-function scan($format){
- stdin_();
-  global $stdin;
-  $out = sscanf($stdin, $format);
-  $stdin = substr($stdin, strlen($out[0]));
-  return $out;
-}
-function scantrim(){
-  global $stdin;
-while(true){
- $stdin = ltrim($stdin);
-if ($stdin != '' || feof(STDIN)) break;
-  stdin_();
-}
-}
 function read_int_line($n){
-  $tab = array();
-  for ($i = 0 ; $i < $n; $i++)
-  {
-    $t = 0;
-    list($t) = scan("%d");
-    scantrim();
-    $tab[$i] = $t;
-  }
-  return $tab;
+  $a = array_map("intval", explode(" ", fgets(STDIN)));
+  return $a;
 }
 
 function read_int_matrix($x, $y){
@@ -34,7 +10,6 @@ function read_int_matrix($x, $y){
   for ($z = 0 ; $z < $y; $z++)
   {
     $out_ = read_int_line($x);
-    scantrim();
     $tab[$z] = $out_;
   }
   return $tab;
@@ -47,8 +22,8 @@ $tab1 = read_int_line($len);
 for ($i = 0 ; $i < $len; $i++)
 {
   echo $i, "=>";
-  $a = $tab1[$i];
-  echo $a, "\n";
+  $b = $tab1[$i];
+  echo $b, "\n";
 }
 $l0 = read_int_line(1);
 $len = $l0[0];
@@ -57,8 +32,8 @@ for ($i = 0 ; $i <= $len - 2; $i++)
 {
   for ($j = 0 ; $j < $len; $j++)
   {
-    $b = $tab2[$i][$j];
-    echo $b, " ";
+    $c = $tab2[$i][$j];
+    echo $c, " ";
   }
   echo "\n";
 }
