12d60f4f3de9a4fcf26f5c174ab0f429cf025c98
Maxime AUDOUIN <coucou747@gmail.com>
2014-03-07 13:48:42 +0100

    make a directory for every compilation pass

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -27,6 +27,7 @@ dispatch begin function
     ocaml_lib "Stdlib/stdlib";
     ocaml_lib "Parser/parser";
     ocaml_lib "Astutils/astutils";
+    ocaml_lib "Passes/passes";
     ocaml_lib "Eval/eval";
     ocaml_lib "Printers/printers";
     dep ["ocaml"; "doc"] & ["docintro.txt"];
