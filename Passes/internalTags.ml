acc684619d782e5db2b87d971454db1a8cbd2454
Maxime AUDOUIN <coucou747@gmail.com>
2014-03-17 23:00:27 +0100

    adding internaltag file

---
+++ Passes/internalTags.ml
@@ -0,0 +1,65 @@
+(*
+ * Copyright (c) 2012, Prologin
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *)
+
+(**
+Cette passe supprime les tags et les inserre dans la map
+@see tags.ml
+    @see <http://prologin.org> Prologin
+    @author Prologin (info\@prologin.org)
+    @author Maxime Audouin (coucou747\@gmail.com)
+*)
+
+open Stdlib
+
+open Ast
+open Fresh
+open PassesUtils
+
+type acc0 = unit
+type 'a acc = unit
+
+let tag s = Tags.tag s
+
+let init_acc () = ()
+
+let rec iter li =
+  List.iter (fun i ->
+		Instr.fold_expr (fun acc e ->
+			Expr.Writer.Deep.iter (fun e ->
+				match Expr.unfix e with
+				| Expr.BinOp(_, Expr.Div, _) -> tag "__internal__div"
+				| Expr.BinOp(_, Expr.Mod, _) -> tag "__internal__mod"
+				| _ -> ()
+			) e
+		) () i
+  ) li
+
+let process acc p =
+  match p with
+  | Prog.DeclarFun (_, _, _, instrs)->
+		iter instrs; acc, p
+  | _ -> acc, p
+
+let process_main acc m =
+	iter m;	acc, m
