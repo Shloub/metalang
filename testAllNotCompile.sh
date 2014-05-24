20029869f4f73cd4e575567f417fa7f32d0b0b8b
Maxime AUDOUIN <coucou747@gmail.com>
2014-05-24 11:43:44 +0200

    removing useless files

--- testAllNotCompile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Tests qui ne doivent pas compiler"
-ocamlbuild Main/main.byte  || exit 1
-for i in `ls tests/not_compile/*.metalang`; do
-    test=`basename "$i" | cut -d . -f 1`
-    echo $test
-    ./main.byte "$i" && exit 1
-done
