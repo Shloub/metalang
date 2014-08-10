3eec092f05cbd2077c6fd85dc8422f6e17cfaf8f
Maxime AUDOUIN <coucou747@gmail.com>
2014-08-10 23:11:21 +0200

    fixing stats script

--- gitstats.plot
+++ gitstats.plot
@@ -1,26 +1,30 @@
 
-set terminal pngcairo  transparent enhanced font "arial,10" fontscale 1.0 size 800, 300
+set terminal pngcairo  transparent enhanced font "arial,10" fontscale 1.0 size 1200, 500
 set output 'size.png'
 set key outside bottom horizontal autotitles nobox
 set title "taille du projet" 
 set grid
 set timefmt "%Y-%m-%d.%H:%M:%S"
-#set logscale y
 set xdata time
+
+set style line 1 lt 2 lw 2 pt 3 ps 0.5
+
+
 plot \
-     'countlines.dat' using 1:2 index 0 title "compiler" with lines,\
-     'countlinesTest.dat' using 1:2 index 0 title "tests" with lines
+     'countlines.dat' using 1:2 index 0 title "compiler" with lines lw 3,\
+     'countlinesTest.dat' using 1:2 index 0 title "tests" with lines lw 3
 
 set output 'langs.png'
 set title "taille du projet" 
 
 plot \
-     'countlinesML.dat' using 1:2 index 0 title "ML" with lines,\
-     'countlinesC.dat' using 1:2 index 0 title "C" with lines,\
-     'countlinesCC.dat' using 1:2 index 0 title "CC" with lines,\
-     'countlinesPY.dat' using 1:2 index 0 title "PY" with lines,\
-     'countlinesPAS.dat' using 1:2 index 0 title "PAS" with lines,\
-     'countlinesGO.dat' using 1:2 index 0 title "GO" with lines,\
-     'countlinesPHP.dat' using 1:2 index 0 title "PHP" with lines,\
-     'countlinesJAVA.dat' using 1:2 index 0 title "JAVA" with lines,\
-     'countlinesCL.dat' using 1:2 index 0 title "CL" with lines
+     'countlinesFUNML.dat' using 1:2 index 0 title "FUNML" with lines lw 2,\
+     'countlinesML.dat' using 1:2 index 0 title "ML" with lines lw 2,\
+     'countlinesC.dat' using 1:2 index 0 title "C" with lines lw 2,\
+     'countlinesCC.dat' using 1:2 index 0 title "CC" with lines lw 2,\
+     'countlinesPY.dat' using 1:2 index 0 title "PY" with lines lw 2,\
+     'countlinesPAS.dat' using 1:2 index 0 title "PAS" with lines lw 2,\
+     'countlinesGO.dat' using 1:2 index 0 title "GO" with lines lw 2,\
+     'countlinesPHP.dat' using 1:2 index 0 title "PHP" with lines lw 2,\
+     'countlinesJAVA.dat' using 1:2 index 0 title "JAVA" with lines lw 2,\
+     'countlinesCL.dat' using 1:2 index 0 title "CL" with lines lw 2
--- gitstats.sh
+++ gitstats.sh
@@ -27,6 +27,7 @@ echo "" > countlinesTest.dat
 echo "" > countlinesC.dat
 echo "" > countlinesCC.dat
 echo "" > countlinesML.dat
+echo "" > countlinesFUNML.dat
 echo "" > countlinesPY.dat
 echo "" > countlinesRB.dat
 echo "" > countlinesJS.dat
@@ -40,73 +41,80 @@ echo "" > countlinesCS.dat
 makepoints(){
     statComp=`countml`
     echo "$1 $statComp" >> countlines.dat
-    if [ -d ./tests/prog ]; then
-    statMETALANG=`counttestschars`
-    statMETALANGL=`counttestslines`
-    statML=`count '*.ml'`
-    statC=`count '*.c'`
-    statCC=`count '*.cc'`
-    statPY=`count '*.py'`
-    statRB=`count '*.rb'`
-    statJS=`count '*.js'`
-    statPAS=`count '*.pas'`
-    statGO=`count '*.go'`
-    statPHP=`count '*.php'`
-    statJAVA=`count '*.java'`
-    statCS=`count '*.cs'`
-    statCL=`count '*.cl'`
-    statML=$(( $statML * 1000 / $statMETALANG ))
-    statC=$(( $statC * 1000 / $statMETALANG ))
-    statCC=$(( $statCC * 1000 / $statMETALANG ))
-    statPY=$(( $statPY * 1000 / $statMETALANG ))
-    statRB=$(( $statRB * 1000 / $statMETALANG ))
-    statJS=$(( $statJS * 1000 / $statMETALANG ))
-    statPAS=$(( $statPAS * 1000 / $statMETALANG ))
-    statGO=$(( $statGO * 1000 / $statMETALANG ))
-    statPHP=$(( $statPHP * 1000 / $statMETALANG ))
-    statJAVA=$(( $statJAVA * 1000 / $statMETALANG ))
-    statCS=$(( $statCS * 1000 / $statMETALANG ))
-    statCL=$(( $statCL * 1000 / $statMETALANG ))
 
-    if [ "$statMETALANGL" != "0" ]; then
-	echo "$1 $statMETALANGL" >> countlinesTest.dat
-    fi
-    if [ "$statML" != "0" ]; then
-	echo "$1 $statML" >> countlinesML.dat
-    fi
-    if [ "$statC" != "0" ]; then
-    echo "$1 $statC" >> countlinesC.dat
-    fi
-    if [ "$statCC" != "0" ]; then
-    echo "$1 $statCC" >> countlinesCC.dat
-    fi
-    if [ "$statPY" != "0" ]; then
-    echo "$1 $statPY" >> countlinesPY.dat
-    fi
-    if [ "$statRB" != "0" ]; then
-    echo "$1 $statRB" >> countlinesRB.dat
-    fi
-    if [ "$statJS" != "0" ]; then
-    echo "$1 $statJS" >> countlinesJS.dat
-    fi
-    if [ "$statPAS" != "0" ]; then
-    echo "$1 $statPAS" >> countlinesPAS.dat
-    fi
-    if [ "$statGO" != "0" ]; then
-    echo "$1 $statGO" >> countlinesGO.dat
-    fi
-    if [ "$statPHP" != "0" ]; then
-    echo "$1 $statPHP" >> countlinesPHP.dat
-    fi
-    if [ "$statJAVA" != "0" ]; then
-    echo "$1 $statJAVA" >> countlinesJAVA.dat
-    fi
-    if [ "$statCS" != "0" ]; then
-    echo "$1 $statCS" >> countlinesCS.dat
-    fi
-    if [ "$statCL" != "0" ]; then
-    echo "$1 $statCL" >> countlinesCL.dat
-    fi
+    if [ -d ./tests/prog ]; then
+        statMETALANG=`counttestschars`
+        statMETALANGL=`counttestslines`
+        statML=`count '*.ml'`
+        statFUNML=`count '*.fun.ml'`
+        statC=`count '*.c'`
+        statCC=`count '*.cc'`
+        statPY=`count '*.py'`
+        statRB=`count '*.rb'`
+        statJS=`count '*.js'`
+        statPAS=`count '*.pas'`
+        statGO=`count '*.go'`
+        statPHP=`count '*.php'`
+        statJAVA=`count '*.java'`
+        statCS=`count '*.cs'`
+        statCL=`count '*.cl'`
+        if [ "$statMETALANGL" != "0" ]; then
+            echo "$1 $statMETALANGL" >> countlinesTest.dat
+        fi
+        if [ "$statMETALANG" != "0" ]; then
+            statML=$(( ( $statML - $statFUNML ) * 1000 / $statMETALANG ))
+            statFUNML=$(( $statFUNML * 1000 / $statMETALANG ))
+            statC=$(( $statC * 1000 / $statMETALANG ))
+            statCC=$(( $statCC * 1000 / $statMETALANG ))
+            statPY=$(( $statPY * 1000 / $statMETALANG ))
+            statRB=$(( $statRB * 1000 / $statMETALANG ))
+            statJS=$(( $statJS * 1000 / $statMETALANG ))
+            statPAS=$(( $statPAS * 1000 / $statMETALANG ))
+            statGO=$(( $statGO * 1000 / $statMETALANG ))
+            statPHP=$(( $statPHP * 1000 / $statMETALANG ))
+            statJAVA=$(( $statJAVA * 1000 / $statMETALANG ))
+            statCS=$(( $statCS * 1000 / $statMETALANG ))
+            statCL=$(( $statCL * 1000 / $statMETALANG ))
+            if [ "$statFUNML" != "0" ]; then
+	              echo "$1 $statFUNML" >> countlinesFUNML.dat
+            fi
+            if [ "$statML" != "0" ]; then
+	              echo "$1 $statML" >> countlinesML.dat
+            fi
+            if [ "$statC" != "0" ]; then
+                echo "$1 $statC" >> countlinesC.dat
+            fi
+            if [ "$statCC" != "0" ]; then
+                echo "$1 $statCC" >> countlinesCC.dat
+            fi
+            if [ "$statPY" != "0" ]; then
+                echo "$1 $statPY" >> countlinesPY.dat
+            fi
+            if [ "$statRB" != "0" ]; then
+                echo "$1 $statRB" >> countlinesRB.dat
+            fi
+            if [ "$statJS" != "0" ]; then
+                echo "$1 $statJS" >> countlinesJS.dat
+            fi
+            if [ "$statPAS" != "0" ]; then
+                echo "$1 $statPAS" >> countlinesPAS.dat
+            fi
+            if [ "$statGO" != "0" ]; then
+                echo "$1 $statGO" >> countlinesGO.dat
+            fi
+            if [ "$statPHP" != "0" ]; then
+                echo "$1 $statPHP" >> countlinesPHP.dat
+            fi
+            if [ "$statJAVA" != "0" ]; then
+                echo "$1 $statJAVA" >> countlinesJAVA.dat
+            fi
+            if [ "$statCS" != "0" ]; then
+                echo "$1 $statCS" >> countlinesCS.dat
+            fi
+            if [ "$statCL" != "0" ]; then
+                echo "$1 $statCL" >> countlinesCL.dat
+            fi
+        fi
     fi
 }
 
@@ -114,8 +122,8 @@ for commit in `walk`; do
     commitdate=`echo $commit | cut -d '_' -f 2`
     commit=`echo $commit | cut -d '_' -f 1`
     echo "$commit $commitdate"
-    git checkout "$commit"
-    makepoints "$commitdate"
+    git checkout "$commit" || exit 1
+    makepoints "$commitdate" || exit 1
 done
 
 git checkout "$origbranch"
