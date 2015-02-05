cb0ffae3d9efef5143ca7c82f69458a1d153c598
Maxime AUDOUIN <coucou747@gmail.com>
2015-02-05 14:00:25 +0100

    updating a tool for prologin

--- metalang2prologin.sh
+++ metalang2prologin.sh
@@ -7,14 +7,23 @@ rename () {
         extension="${i##*.}"
         if [ "$extension" = "$2" ] ; then
             git mv "$1/$filename.$2" "$1/$filename.$3"
+            mv "$1/$filename.$2" "$1/$filename.$3"
         fi
     done
 
 }
 
+indir() {
+    rename $1 "py" "py3"
+    rename $1 "ml" "ocaml"
+    rename $1 "cc" "c++"
+}
+
+indir "."
+
 for i in `ls`; do
-    rename $i "py" "py3"
-    rename $i "ml" "ocaml"
-    rename $i "cc" "c++"
+    if [ -d "$i" ]; then
+        indir "$i"
+    fi
 done
 
