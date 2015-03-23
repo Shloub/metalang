4cd68b286e58a0c04976713790d39ea6584a7fba
Audouin Maxime <coucou747@gmail.com>
2015-03-23 02:19:21 +0100

    updating tests

--- out/sumDiv.fs
+++ out/sumDiv.fs
@@ -1,16 +1,4 @@
-
-
-: // { a b }
-  a b /
-  a 0 < b 0 < XOR IF 1 + THEN
-;
-
-: % { a b }
-  a b MOD
-  a 0 < b 0 < XOR IF b - THEN
- ;
-
-
+: % { a b } a b MOD a 0 < b 0 < XOR IF b - THEN ;
 VARIABLE buffer-index
 1000 buffer-index !
 VARIABLE NEOF
@@ -18,41 +6,20 @@ VARIABLE NEOF
 VARIABLE buffer-max
 0 buffer-max !
 create bufferc 128 allot
-
 : next-char
   buffer-index @ 1 + buffer-index !
   buffer-index @ buffer-max @ > IF
     0 buffer-index !
     bufferc 128 stdin read-line DROP -1 = NEOF ! buffer-max !
     10 bufferc buffer-max @ + !
-  THEN
-;
-
+  THEN ;
 : current-char
   buffer-index @ buffer-max @ > IF next-char THEN
   bufferc buffer-index @ + c@ ;
-
-: skipspaces
-  BEGIN NEOF @ current-char 13 = current-char 32 = OR current-char 10 = OR AND
-  WHILE next-char REPEAT
-;
-
 : read-int
-  [char] - current-char = IF
-    -1
-    next-char
-  ELSE 1
-  THEN
-  0
-  BEGIN current-char [char] 0 >= current-char [char] 9 <= AND
-  WHILE 10 * current-char [char] 0 - + next-char REPEAT
-  *
-;
-
-: read-char current-char next-char ;
-
-
-
+  [char] - current-char = IF -1 next-char ELSE 1 THEN
+  0 BEGIN current-char [char] 0 >= current-char [char] 9 <= AND
+  WHILE 10 * current-char [char] 0 - + next-char REPEAT * ;
 : foo {  }
   0 { a }
   \  test 
