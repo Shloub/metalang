d39fde28bd804509109db11275d3df7db55f6710
Audouin Maxime <coucou747@gmail.com>
2015-03-23 01:33:45 +0100

    Forth : fixing apply & call

--- Printers/forthPrinter.ml
+++ Printers/forthPrinter.ml
@@ -213,7 +213,7 @@ create bufferc 128 allot
 
   method hasSelfAffect op = false
 
-  method call f var li =
+  method apply f var li =
     match StringMap.find_opt var macros with
     | Some ( (t, params, code) ) ->
       self#expand_macro_call f var t params code li
