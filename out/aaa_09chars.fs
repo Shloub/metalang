b67383246155dd38cef8c212bb9f9357adee6e94
Audouin Maxime <coucou747@gmail.com>
2015-03-27 21:33:18 +0100

    adding generated files for test aaa_09chars

---
+++ out/aaa_09chars.fs
@@ -0,0 +1,513 @@
+: main
+  1 s>d 0 d.r
+   s" =>" TYPE
+  1 EMIT
+  NEWLINE TYPE
+  2 s>d 0 d.r
+   s" =>" TYPE
+  2 EMIT
+  NEWLINE TYPE
+  3 s>d 0 d.r
+   s" =>" TYPE
+  3 EMIT
+  NEWLINE TYPE
+  4 s>d 0 d.r
+   s" =>" TYPE
+  4 EMIT
+  NEWLINE TYPE
+  5 s>d 0 d.r
+   s" =>" TYPE
+  5 EMIT
+  NEWLINE TYPE
+  6 s>d 0 d.r
+   s" =>" TYPE
+  6 EMIT
+  NEWLINE TYPE
+  7 s>d 0 d.r
+   s" =>" TYPE
+  7 EMIT
+  NEWLINE TYPE
+  8 s>d 0 d.r
+   s" =>" TYPE
+  8 EMIT
+  NEWLINE TYPE
+  9 s>d 0 d.r
+   s" =>" TYPE
+  9 EMIT
+  NEWLINE TYPE
+  10 s>d 0 d.r
+   s" =>" TYPE
+  10 EMIT
+  NEWLINE TYPE
+  11 s>d 0 d.r
+   s" =>" TYPE
+  11 EMIT
+  NEWLINE TYPE
+  12 s>d 0 d.r
+   s" =>" TYPE
+  12 EMIT
+  NEWLINE TYPE
+  13 s>d 0 d.r
+   s" =>" TYPE
+  13 EMIT
+  NEWLINE TYPE
+  14 s>d 0 d.r
+   s" =>" TYPE
+  14 EMIT
+  NEWLINE TYPE
+  15 s>d 0 d.r
+   s" =>" TYPE
+  15 EMIT
+  NEWLINE TYPE
+  16 s>d 0 d.r
+   s" =>" TYPE
+  16 EMIT
+  NEWLINE TYPE
+  17 s>d 0 d.r
+   s" =>" TYPE
+  17 EMIT
+  NEWLINE TYPE
+  18 s>d 0 d.r
+   s" =>" TYPE
+  18 EMIT
+  NEWLINE TYPE
+  19 s>d 0 d.r
+   s" =>" TYPE
+  19 EMIT
+  NEWLINE TYPE
+  20 s>d 0 d.r
+   s" =>" TYPE
+  20 EMIT
+  NEWLINE TYPE
+  21 s>d 0 d.r
+   s" =>" TYPE
+  21 EMIT
+  NEWLINE TYPE
+  22 s>d 0 d.r
+   s" =>" TYPE
+  22 EMIT
+  NEWLINE TYPE
+  23 s>d 0 d.r
+   s" =>" TYPE
+  23 EMIT
+  NEWLINE TYPE
+  24 s>d 0 d.r
+   s" =>" TYPE
+  24 EMIT
+  NEWLINE TYPE
+  25 s>d 0 d.r
+   s" =>" TYPE
+  25 EMIT
+  NEWLINE TYPE
+  26 s>d 0 d.r
+   s" =>" TYPE
+  26 EMIT
+  NEWLINE TYPE
+  27 s>d 0 d.r
+   s" =>" TYPE
+  27 EMIT
+  NEWLINE TYPE
+  28 s>d 0 d.r
+   s" =>" TYPE
+  28 EMIT
+  NEWLINE TYPE
+  29 s>d 0 d.r
+   s" =>" TYPE
+  29 EMIT
+  NEWLINE TYPE
+  30 s>d 0 d.r
+   s" =>" TYPE
+  30 EMIT
+  NEWLINE TYPE
+  31 s>d 0 d.r
+   s" =>" TYPE
+  31 EMIT
+  NEWLINE TYPE
+  32 s>d 0 d.r
+   s" =>" TYPE
+  32 EMIT
+  NEWLINE TYPE
+  33 s>d 0 d.r
+   s" =>" TYPE
+  33 EMIT
+  NEWLINE TYPE
+  34 s>d 0 d.r
+   s" =>" TYPE
+  34 EMIT
+  NEWLINE TYPE
+  35 s>d 0 d.r
+   s" =>" TYPE
+  35 EMIT
+  NEWLINE TYPE
+  36 s>d 0 d.r
+   s" =>" TYPE
+  36 EMIT
+  NEWLINE TYPE
+  37 s>d 0 d.r
+   s" =>" TYPE
+  37 EMIT
+  NEWLINE TYPE
+  38 s>d 0 d.r
+   s" =>" TYPE
+  38 EMIT
+  NEWLINE TYPE
+  39 s>d 0 d.r
+   s" =>" TYPE
+  39 EMIT
+  NEWLINE TYPE
+  40 s>d 0 d.r
+   s" =>" TYPE
+  40 EMIT
+  NEWLINE TYPE
+  41 s>d 0 d.r
+   s" =>" TYPE
+  41 EMIT
+  NEWLINE TYPE
+  42 s>d 0 d.r
+   s" =>" TYPE
+  42 EMIT
+  NEWLINE TYPE
+  43 s>d 0 d.r
+   s" =>" TYPE
+  43 EMIT
+  NEWLINE TYPE
+  44 s>d 0 d.r
+   s" =>" TYPE
+  44 EMIT
+  NEWLINE TYPE
+  45 s>d 0 d.r
+   s" =>" TYPE
+  45 EMIT
+  NEWLINE TYPE
+  46 s>d 0 d.r
+   s" =>" TYPE
+  46 EMIT
+  NEWLINE TYPE
+  47 s>d 0 d.r
+   s" =>" TYPE
+  47 EMIT
+  NEWLINE TYPE
+  48 s>d 0 d.r
+   s" =>" TYPE
+  [char] 0 EMIT
+  NEWLINE TYPE
+  49 s>d 0 d.r
+   s" =>" TYPE
+  [char] 1 EMIT
+  NEWLINE TYPE
+  50 s>d 0 d.r
+   s" =>" TYPE
+  [char] 2 EMIT
+  NEWLINE TYPE
+  51 s>d 0 d.r
+   s" =>" TYPE
+  [char] 3 EMIT
+  NEWLINE TYPE
+  52 s>d 0 d.r
+   s" =>" TYPE
+  [char] 4 EMIT
+  NEWLINE TYPE
+  53 s>d 0 d.r
+   s" =>" TYPE
+  [char] 5 EMIT
+  NEWLINE TYPE
+  54 s>d 0 d.r
+   s" =>" TYPE
+  [char] 6 EMIT
+  NEWLINE TYPE
+  55 s>d 0 d.r
+   s" =>" TYPE
+  [char] 7 EMIT
+  NEWLINE TYPE
+  56 s>d 0 d.r
+   s" =>" TYPE
+  [char] 8 EMIT
+  NEWLINE TYPE
+  57 s>d 0 d.r
+   s" =>" TYPE
+  [char] 9 EMIT
+  NEWLINE TYPE
+  58 s>d 0 d.r
+   s" =>" TYPE
+  [char] : EMIT
+  NEWLINE TYPE
+  59 s>d 0 d.r
+   s" =>" TYPE
+  [char] ; EMIT
+  NEWLINE TYPE
+  60 s>d 0 d.r
+   s" =>" TYPE
+  [char] < EMIT
+  NEWLINE TYPE
+  61 s>d 0 d.r
+   s" =>" TYPE
+  [char] = EMIT
+  NEWLINE TYPE
+  62 s>d 0 d.r
+   s" =>" TYPE
+  [char] > EMIT
+  NEWLINE TYPE
+  63 s>d 0 d.r
+   s" =>" TYPE
+  [char] ? EMIT
+  NEWLINE TYPE
+  64 s>d 0 d.r
+   s" =>" TYPE
+  [char] @ EMIT
+  NEWLINE TYPE
+  65 s>d 0 d.r
+   s" =>" TYPE
+  [char] A EMIT
+  NEWLINE TYPE
+  66 s>d 0 d.r
+   s" =>" TYPE
+  [char] B EMIT
+  NEWLINE TYPE
+  67 s>d 0 d.r
+   s" =>" TYPE
+  [char] C EMIT
+  NEWLINE TYPE
+  68 s>d 0 d.r
+   s" =>" TYPE
+  [char] D EMIT
+  NEWLINE TYPE
+  69 s>d 0 d.r
+   s" =>" TYPE
+  [char] E EMIT
+  NEWLINE TYPE
+  70 s>d 0 d.r
+   s" =>" TYPE
+  [char] F EMIT
+  NEWLINE TYPE
+  71 s>d 0 d.r
+   s" =>" TYPE
+  [char] G EMIT
+  NEWLINE TYPE
+  72 s>d 0 d.r
+   s" =>" TYPE
+  [char] H EMIT
+  NEWLINE TYPE
+  73 s>d 0 d.r
+   s" =>" TYPE
+  [char] I EMIT
+  NEWLINE TYPE
+  74 s>d 0 d.r
+   s" =>" TYPE
+  [char] J EMIT
+  NEWLINE TYPE
+  75 s>d 0 d.r
+   s" =>" TYPE
+  [char] K EMIT
+  NEWLINE TYPE
+  76 s>d 0 d.r
+   s" =>" TYPE
+  [char] L EMIT
+  NEWLINE TYPE
+  77 s>d 0 d.r
+   s" =>" TYPE
+  [char] M EMIT
+  NEWLINE TYPE
+  78 s>d 0 d.r
+   s" =>" TYPE
+  [char] N EMIT
+  NEWLINE TYPE
+  79 s>d 0 d.r
+   s" =>" TYPE
+  [char] O EMIT
+  NEWLINE TYPE
+  80 s>d 0 d.r
+   s" =>" TYPE
+  [char] P EMIT
+  NEWLINE TYPE
+  81 s>d 0 d.r
+   s" =>" TYPE
+  [char] Q EMIT
+  NEWLINE TYPE
+  82 s>d 0 d.r
+   s" =>" TYPE
+  [char] R EMIT
+  NEWLINE TYPE
+  83 s>d 0 d.r
+   s" =>" TYPE
+  [char] S EMIT
+  NEWLINE TYPE
+  84 s>d 0 d.r
+   s" =>" TYPE
+  [char] T EMIT
+  NEWLINE TYPE
+  85 s>d 0 d.r
+   s" =>" TYPE
+  [char] U EMIT
+  NEWLINE TYPE
+  86 s>d 0 d.r
+   s" =>" TYPE
+  [char] V EMIT
+  NEWLINE TYPE
+  87 s>d 0 d.r
+   s" =>" TYPE
+  [char] W EMIT
+  NEWLINE TYPE
+  88 s>d 0 d.r
+   s" =>" TYPE
+  [char] X EMIT
+  NEWLINE TYPE
+  89 s>d 0 d.r
+   s" =>" TYPE
+  [char] Y EMIT
+  NEWLINE TYPE
+  90 s>d 0 d.r
+   s" =>" TYPE
+  [char] Z EMIT
+  NEWLINE TYPE
+  91 s>d 0 d.r
+   s" =>" TYPE
+  [char] [ EMIT
+  NEWLINE TYPE
+  92 s>d 0 d.r
+   s" =>" TYPE
+  [char] \ EMIT
+  NEWLINE TYPE
+  93 s>d 0 d.r
+   s" =>" TYPE
+  [char] ] EMIT
+  NEWLINE TYPE
+  94 s>d 0 d.r
+   s" =>" TYPE
+  [char] ^ EMIT
+  NEWLINE TYPE
+  95 s>d 0 d.r
+   s" =>" TYPE
+  [char] _ EMIT
+  NEWLINE TYPE
+  96 s>d 0 d.r
+   s" =>" TYPE
+  [char] ` EMIT
+  NEWLINE TYPE
+  97 s>d 0 d.r
+   s" =>" TYPE
+  [char] a EMIT
+  NEWLINE TYPE
+  98 s>d 0 d.r
+   s" =>" TYPE
+  [char] b EMIT
+  NEWLINE TYPE
+  99 s>d 0 d.r
+   s" =>" TYPE
+  [char] c EMIT
+  NEWLINE TYPE
+  100 s>d 0 d.r
+   s" =>" TYPE
+  [char] d EMIT
+  NEWLINE TYPE
+  101 s>d 0 d.r
+   s" =>" TYPE
+  [char] e EMIT
+  NEWLINE TYPE
+  102 s>d 0 d.r
+   s" =>" TYPE
+  [char] f EMIT
+  NEWLINE TYPE
+  103 s>d 0 d.r
+   s" =>" TYPE
+  [char] g EMIT
+  NEWLINE TYPE
+  104 s>d 0 d.r
+   s" =>" TYPE
+  [char] h EMIT
+  NEWLINE TYPE
+  105 s>d 0 d.r
+   s" =>" TYPE
+  [char] i EMIT
+  NEWLINE TYPE
+  106 s>d 0 d.r
+   s" =>" TYPE
+  [char] j EMIT
+  NEWLINE TYPE
+  107 s>d 0 d.r
+   s" =>" TYPE
+  [char] k EMIT
+  NEWLINE TYPE
+  108 s>d 0 d.r
+   s" =>" TYPE
+  [char] l EMIT
+  NEWLINE TYPE
+  109 s>d 0 d.r
+   s" =>" TYPE
+  [char] m EMIT
+  NEWLINE TYPE
+  110 s>d 0 d.r
+   s" =>" TYPE
+  [char] n EMIT
+  NEWLINE TYPE
+  111 s>d 0 d.r
+   s" =>" TYPE
+  [char] o EMIT
+  NEWLINE TYPE
+  112 s>d 0 d.r
+   s" =>" TYPE
+  [char] p EMIT
+  NEWLINE TYPE
+  113 s>d 0 d.r
+   s" =>" TYPE
+  [char] q EMIT
+  NEWLINE TYPE
+  114 s>d 0 d.r
+   s" =>" TYPE
+  [char] r EMIT
+  NEWLINE TYPE
+  115 s>d 0 d.r
+   s" =>" TYPE
+  [char] s EMIT
+  NEWLINE TYPE
+  116 s>d 0 d.r
+   s" =>" TYPE
+  [char] t EMIT
+  NEWLINE TYPE
+  117 s>d 0 d.r
+   s" =>" TYPE
+  [char] u EMIT
+  NEWLINE TYPE
+  118 s>d 0 d.r
+   s" =>" TYPE
+  [char] v EMIT
+  NEWLINE TYPE
+  119 s>d 0 d.r
+   s" =>" TYPE
+  [char] w EMIT
+  NEWLINE TYPE
+  120 s>d 0 d.r
+   s" =>" TYPE
+  [char] x EMIT
+  NEWLINE TYPE
+  121 s>d 0 d.r
+   s" =>" TYPE
+  [char] y EMIT
+  NEWLINE TYPE
+  122 s>d 0 d.r
+   s" =>" TYPE
+  [char] z EMIT
+  NEWLINE TYPE
+  123 s>d 0 d.r
+   s" =>" TYPE
+  [char] { EMIT
+  NEWLINE TYPE
+  124 s>d 0 d.r
+   s" =>" TYPE
+  [char] | EMIT
+  NEWLINE TYPE
+  125 s>d 0 d.r
+   s" =>" TYPE
+  [char] } EMIT
+  NEWLINE TYPE
+  126 s>d 0 d.r
+   s" =>" TYPE
+  [char] ~ EMIT
+  NEWLINE TYPE
+  127 s>d 0 d.r
+   s" =>" TYPE
+  [char]  EMIT
+  NEWLINE TYPE
+  ;
+main
+BYE
+
