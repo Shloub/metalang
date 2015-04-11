745ac6693176b78b7fcf213cddcd6023a72edb2c
Audouin Maxime <coucou747@gmail.com>
2015-04-11 17:39:29 +0200

    removing AstFun.Expr.Writer module. Now we use AstFun.Expr.Fixed

--- Passesfun/detectSideEffect.ml
+++ Passesfun/detectSideEffect.ml
@@ -35,56 +35,56 @@ open AstFun
    @author Maxime Audouin (coucou747\@gmail.com)
 *)
 
-let has_side_effects acc e = IntMap.find (Expr.Fixed.annot e) acc
+let (||) a b = match a, b with
+  | EEffect, _ -> EEffect
+  | _, EEffect -> EEffect
+  | _ -> EPure
 
-let side_effects acc e =
-  let has = match Expr.unfix e with
-    | Expr.Skip -> true
-    | Expr.LetRecIn (_, _, e1, e2) ->
-      has_side_effects acc e1 || has_side_effects acc e2
-    | Expr.UnOp (e, _) -> has_side_effects acc e
-    | Expr.BinOp (a, _, b) ->
-      has_side_effects acc a || has_side_effects acc b
-    | Expr.Fun _ -> false
-    | Expr.FunTuple _ -> false
-    | Expr.Apply (a, li) ->
-        begin match Expr.unfix a with
-        | Expr.Lief (Expr.Binding (Ast.UserName a)) ->
-            if Tags.is_taged ("macro_" ^ a ^ "_pure")
-            then
-              List.exists (has_side_effects acc) li
-            else true
-        | _ -> true
-        end
-    | Expr.Tuple li ->
-      List.exists (has_side_effects acc) li
-    | Expr.Lief _ -> false
-    | Expr.Comment (_, e) -> has_side_effects acc e
-    | Expr.If (a, b, c) ->
-      has_side_effects acc a ||
-        has_side_effects acc b ||
-        has_side_effects acc c
-    | Expr.Print _ -> true
-    | Expr.ReadIn _ -> true
-    | Expr.Block li -> List.exists (has_side_effects acc) li
-    | Expr.Record li -> true
-    | Expr.RecordAffect _ -> true
-    | Expr.RecordAccess _ -> true
-    | Expr.ArrayInit _ -> true
-    | Expr.ArrayMake (a, b, c) -> true
-    | Expr.ArrayAccess _ -> true
-    | Expr.ArrayAffect _ -> true
-    | Expr.LetIn (binding, e1, e) -> has_side_effects acc e || has_side_effects acc e1
-    | Expr.MultiPrint (_, _) -> true
+let merge acc1 acc2 = IntMap.merge (fun key e1 e2 -> match e1, e2 with
+						     | Some e1, _ -> Some e1
+						     | None, Some e2 -> Some e2
+						     | _ -> None ) acc1 acc2
 
-  in IntMap.add (Expr.Fixed.annot e) has acc
+let side_effects i e =
+  let v, acc = match e with
+  | Expr.Fun (_, (_, acc)) -> EPure, acc
+  | Expr.FunTuple (_, (_, acc)) -> EPure, acc
+  | Expr.Lief (Expr.Binding (Ast.UserName a)) when Tags.is_taged ("macro_" ^ a ^ "_pure") -> EMacro, IntMap.empty
+  | Expr.Lief _ -> EPure, IntMap.empty
+  | Expr.Apply ((EMacro, _), _)
+  | Expr.LetRecIn _
+  | Expr.UnOp _
+  | Expr.BinOp _
+  | Expr.Tuple _
+  | Expr.Comment _
+  | Expr.LetIn _
+  | Expr.If _
+  | Expr.Block _ -> Expr.Fixed.Surface.fold
+		      (fun (has, acc) (has2, acc2) ->
+		       has || has2,
+		       merge acc acc2) (EPure, IntMap.empty) e
+  | Expr.Skip
+  | Expr.Apply _
+  | Expr.MultiPrint _
+  | Expr.RecordAffect _
+  | Expr.RecordAccess _
+  | Expr.ArrayInit _
+  | Expr.ArrayMake _
+  | Expr.ArrayAccess _
+  | Expr.ArrayAffect _
+  | Expr.Print _
+  | Expr.ReadIn _
+  | Expr.Record _ ->
+     let acc = Expr.Fixed.Surface.fold (fun acc2 (_, acc) -> merge acc acc2) IntMap.empty e in
+    EEffect, acc
+  in v, IntMap.add i v acc
 
-let tr acc e = Expr.Writer.Deep.fold side_effects acc e
+let tr e = snd (Expr.Fixed.Deep.folda side_effects e)
 
 let apply p =
   let side_effects = List.fold_left (fun acc e -> match e with
-  | Declaration (name, e) -> tr acc e
-	| _ -> acc
+  | Declaration (name, e) -> merge acc (tr e)
+  | _ -> acc
   ) p.side_effects p.declarations
   in {p with side_effects = side_effects }
 
