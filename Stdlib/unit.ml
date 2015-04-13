f1bc29b5d67815dcb6637575c2fd7e381473fd5b
Audouin Maxime <coucou747@gmail.com>
2015-04-14 00:26:58 +0200

    adding a small unit test

--- Stdlib/unit.ml
+++ Stdlib/unit.ml
@@ -3,4 +3,5 @@ let () = begin
   let a = Stdlib.String.replace "o" "\\o" "toto" in
   Format.printf "a=%S\n" a;
   assert (a = "t\\ot\\o");
+  assert (Stdlib.List.mapi (fun i a -> a + i) [0 ; 1; 10; 15] = [0; 2; 12; 18] );
 end
