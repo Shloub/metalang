bf2e5cb4dc1fd3d53404bed786c36e3e59539d09
Sacha Delanoue <contact@shaac.me>
2016-01-24 19:56:31 +0100

    Use hardcoded cpp read_char_matrix

--- out/prologin_template_charmatrix.cpp
+++ out/prologin_template_charmatrix.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <vector>
-std::vector<char> getline(){
-  if (std::cin.flags() & std::ios_base::skipws){
-    char c = std::cin.peek();
-    if (c == '\n' || c == ' ') std::cin.ignore();
-    std::cin.unsetf(std::ios::skipws);
+template <typename T> std::vector<std::vector<T> > read_matrix(int x, int y){
+  std::vector<std::vector<T> > matrix(y);
+  for (int i = 0; i < y; i++)
+  {
+    std::vector<T>& line = matrix[i];
+    line.resize(x);
+    for (int j = 0; j < x; j++)
+    {
+        std::cin >> line[j] >> std::skipws;
+    }
   }
-  std::string line;
-  std::getline(std::cin, line);
-  std::vector<char> c(line.begin(), line.end());
-  return c;
+  return matrix;
 }
 int programme_candidat(std::vector<std::vector<char > >& tableau, int taille_x, int taille_y){
   int out0 = 0;
@@ -29,10 +31,7 @@ int programme_candidat(std::vector<std::vector<char > >& tableau, int taille_x, 
 int main(){
   int taille_y, taille_x;
   std::cin >> taille_x >> std::skipws >> taille_y;
-  std::vector<std::vector<char > > a(taille_y);
-  for (int b = 0 ; b < taille_y; b++)
-    a[b] = getline();
-  std::vector<std::vector<char > > tableau = a;
+  std::vector<std::vector<char > > tableau = read_matrix<char>(taille_x, taille_y);
   std::cout << programme_candidat(tableau, taille_x, taille_y) << "\n";
   return 0;
 }
