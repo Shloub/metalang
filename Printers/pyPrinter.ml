f0d609a0dda0d0b7041d8249baa225119198e583
Audouin Maxime <coucou747@gmail.com>
2016-06-22 22:53:29 +0200

    python printer refactoring WIP

--- Printers/pyPrinter.ml
+++ Printers/pyPrinter.ml
@@ -45,7 +45,7 @@ let print_lief prio f l =
     | Bool false -> fprintf f "False"
     | x -> print_lief prio f x
 
-let print_expr macros e f p =
+let config macros =
   let open Format in
   let open Expr in
   let prio_binop op = match op with
@@ -62,14 +62,6 @@ let print_expr macros e f p =
   | Diff -> nonassocl 13
   | And -> assoc 15
   | Or -> assoc 15 in
-  let print_expr0 config e f prio_parent = match e with
-  | BinOp (a, (Div as op), b) ->
-      let _, priol, prior = prio_binop op in
-      fprintf f "math.trunc(%a %a %a)" a priol print_op op b prior
-  | BinOp (a, Mod, b) -> fprintf f "mod(%a, %a)" a nop b nop
-  | Tuple li -> fprintf f "[%a]" (print_list (fun f x -> x f nop) sep_c) li
-  | _ -> print_expr0 config e f prio_parent
-  in
   let print_op f op =
     fprintf f (match op with
     | Add -> "+"
@@ -90,7 +82,7 @@ let print_expr macros e f p =
     Format.fprintf f (match op with
     | Neg -> "-"
     | Not -> "not ") in
-  let config = {
+  {
     prio_binop;
     prio_unop;
     print_varname;
@@ -99,20 +91,131 @@ let print_expr macros e f p =
     print_unop;
     print_mut;
     macros
-  } in Fixed.Deep.fold (print_expr0 config) e f p
+  }
 
+let print_expr config e f p =
+  let open Format in
+  let open Expr in
+  let print_expr0 config e f prio_parent = match e with
+  | BinOp (a, (Div as op), b) ->
+      let _, priol, prior = prio_binop op in
+      fprintf f "math.trunc(%a %a %a)" a priol print_op op b prior
+  | BinOp (a, Mod, b) -> fprintf f "mod(%a, %a)" a nop b nop
+  | Tuple li -> fprintf f "[%a]" (print_list (fun f x -> x f nop) sep_c) li
+  | _ -> print_expr0 config e f prio_parent
+  in Fixed.Deep.fold (print_expr0 config) e f p
+
+let print_instr tyenv c i =
+  let open Ast.Instr in
+  let open Format in
+  let block value f li =
+    if List.for_all (fun i -> i.is_comment) li then fprintf f "@\npass"
+    else fprintf f "@\n%a" (print_list (fun f i -> i.p f (false, value)) sep_nl) li in
+  let block_lambda = block true in
+  let read t pp f =
+    match Type.unfix t with
+    | Type.Integer -> fprintf f "@[<h>%a = readint()@]" pp ()
+    | Type.Char -> fprintf f "@[<h>%a = readchar()@]" pp ()
+    | _ -> assert false (* type non géré*) in
+  let p f (inelseif, inlambda) =
+    let block = block inlambda in match i with
+    | Declare (var, ty, e, _) -> fprintf f "%a = %a" c.print_varname var e nop
+    | SelfAffect (mut, Ast.Expr.Div, e) -> fprintf f "%a = math.trunc(%a / %a)"
+          (c.print_mut c nop) mut (c.print_mut c nop) mut e nop
+    | SelfAffect (mut, Ast.Expr.Mod, e) -> fprintf f "%a = mod(%a, %a)"
+          (c.print_mut c nop) mut (c.print_mut c nop) mut e nop
+    | SelfAffect (mut, op, e) -> fprintf f "%a %a= %a" (c.print_mut c nop) mut c.print_op op e nop
+    | Affect (mut, e) -> fprintf f "%a = %a" (c.print_mut c nop) mut e nop
+    | ClikeLoop (init, cond, incr, li) -> assert false
+    | Loop (var, e1, e2, li) -> fprintf f "@[<v 4>@[<h>for %a in range(%a, 1 + %a):@]%a@]"
+          c.print_varname var e1 nop e2 nop
+          block li
+    | While (e, li) -> fprintf f "@[<v 4>while @[<h>%a@]:%a@]" e nop block li
+    | Comment s -> let lic = String.split s '\n' in
+        print_list (fun f s -> Format.fprintf f "#%s@\n" s) nosep f lic
+    | Tag s -> fprintf f "/*%S*/" s
+    | Return e -> if inlambda then fprintf f "next %a" e nop else fprintf f "return %a" e nop
+    | AllocArray (name, t, e, None, opt) ->
+        fprintf f "@[<h>%a@ =@ [None] * %a@]"
+          c.print_varname name
+          (fun f a -> a f (prio_right (prio_binop Ast.Expr.Mul))) e
+    | AllocArray (name, t, e, Some (var, lambda), opt) ->
+        fprintf f "@[<v 2>%a = [*0..@[<h>%a-1@]].map { |%a|@\n%a@\n}@]"
+          c.print_varname name
+          e nop
+          c.print_varname var
+          block_lambda lambda
+    | AllocArrayConst (name, ty, len, lief, opt) ->
+        fprintf f "@[<h>%a@ = [%a] * %a@]"
+          c.print_varname name
+          (c.print_lief nop) lief
+          len (prio_right (prio_binop Ast.Expr.Mul))
+    | AllocRecord (name, ty, list, opt) ->
+        fprintf f "@[<v 4>%a = {%a}@\n@]" c.print_varname name
+          (print_list (fun f (field, x) -> fprintf f "%S:%a" field x nop) sep_nl) list
+    | If (e, listif, []) when inelseif ->
+        fprintf f "if %a:%a@]" e nop block listif
+    | If (e, listif, [elsecase]) when inelseif && elsecase.is_if ->
+        fprintf f "if %a:%a@]@\n@[<v 4>el%a" e nop block listif elsecase.p (true, inlambda)          
+    | If (e, listif, listelse)  when inelseif ->
+        fprintf f "if %a:%a@]@\n@[<v 4>else:%a@]" e nop block listif block listelse
+    | If (e, listif, []) ->
+        fprintf f "@[<v 4>if %a:%a@]" e nop block listif
+    | If (e, listif, [elsecase]) when elsecase.is_if ->
+        fprintf f "@[<v 4>if %a:%a@]@\n@[<v 4>el%a" e nop block listif elsecase.p (true, inlambda)          
+    | If (e, listif, listelse) ->
+        fprintf f "@[<v 4>if %a:%a@]@\n@[<v 4>else:%a@]" e nop block listif block listelse
+    | Call (func, li) ->  begin match StringMap.find_opt func c.macros with
+      | Some ( (t, params, code) ) -> pmacros f "%s" t params code li nop
+      | None -> fprintf f "%s(%a)" func (print_list (fun f x -> x f nop) sep_c) li
+    end
+    | Print li->
+        let format, exprs = extract_multi_print clike_noformat format_type li in
+        (* TODO match end of format *)
+        begin match exprs with
+        | [] -> fprintf f "print(\"%s\", end='')" format
+        | [_ty, e] -> fprintf f "print(\"%s\" %% %a, end='')" format e prio_percent_percent
+        | _ -> fprintf f "print(\"%s\" %% (%a), end='')" format
+              (print_list (fun f (t, e) -> e f nop) sep_c) exprs
+        end
+    | Read li ->
+        let li = List.map (function
+          | Separation -> fun f -> fprintf f "@[<hov>stdinsep()@]"
+          | DeclRead (t, var, _option) -> read t (fun f () -> print_varname f var)
+          | ReadExpr (t, m) -> read t (fun f () -> c.print_mut c nop f m)) li
+        in print_list (fun f e -> e f) sep_nl f li
+    | Untuple (li, expr, opt) -> fprintf f "(%a) = %a" (print_list c.print_varname sep_c) (List.map snd li) expr nop
+    | Unquote e -> assert false in
+  let is_multi_instr = match i with
+  | Read (hd::tl) -> true
+  | _ -> false in
+  {
+   is_multi_instr = is_multi_instr;
+   is_if=is_if i;
+   is_if_noelse=is_if_noelse i;
+   is_comment=is_comment i;
+   p=p;
+   default = (false, false);
+   print_lief = c.print_lief;
+ }
+
+let print_instr tyenv macros i =
+  let open Ast.Instr.Fixed.Deep in
+  let c = config macros in
+  let i = (fold (print_instr tyenv c) (mapg (print_expr c) i))
+  in fun f -> i.p f i.default
+  
 class pyPrinter = object(self)
   inherit CPrinter.cPrinter as super
 
-  method exprp p f e = print_expr
-      (StringMap.map (fun (ty, params, li) ->
+  method instr f (t:Utils.instr) =
+   let macros = StringMap.map (fun (ty, params, li) ->
         ty, params,
-        try List.assoc (self#lang ()) li
-        with Not_found -> List.assoc "" li) macros) e f p
+        try List.assoc "py" li
+        with Not_found -> List.assoc "" li) macros
+   in (print_instr typerEnv macros t) f
 
   method multi_read f li = self#base_multi_read f li
-
-  method expr f e = self#exprp nop f e
 
   method declare_for s f li = ()
   method separator f () = ()
@@ -124,15 +227,6 @@ class pyPrinter = object(self)
   | Expr.Div -> Format.fprintf f "@[<h>%a = math.trunc(%a / %a)@]" self#mutable_set m self#mutable_get m self#expr e2
   | Expr.Mod -> Format.fprintf f "@[<h>%a = mod(%a, %a)@]" self#mutable_set m self#mutable_get m self#expr e2
   | _ -> assert false
-
-  method lang () = "py"
-
-  method allocarrayconst f binding t len e opt =
-    Format.fprintf f "@[<h>%a@ = [%a] * %a@]"
-      self#binding binding
-      (print_lief nop) e
-      (fun f e -> self#exprp (prio_right (prio_binop Ast.Expr.Mul)) f e)
-      len
 
   method read f t mutable_ =
     match Type.unfix t with
@@ -275,35 +369,12 @@ def skipchar():
   method whileloop f expr li =
     Format.fprintf f "@[<h>while (%a):@]@\n%a" self#expr expr self#bloc li
 
-  method allocarray f binding type_ len _ =
-    Format.fprintf f "@[<h>%a@ =@ [None] * %a@]"
-      self#binding binding
-      (fun f a -> self#exprp (prio_right (prio_binop Ast.Expr.Mul)) f a) len
-
   method print_fun f funname t li instrs =
     Format.fprintf f "@[<h>%a@]@\n@[<v 4>%a@]@\n"
       self#print_proto (funname, t, li)
       self#bloc instrs
 
   method decl_type f name t = ()
-
-  method forloop f varname expr1 expr2 li = self#forloop_content f (varname, expr1, expr2, li)
-
-  method forloop_content f (varname, expr1, expr2, li) =
-    let default () =
-      Format.fprintf f "@[<h>for@ %a@ in@ range(%a,@ 1 + %a):@\n@]%a"
-        self#binding varname
-        self#expr expr1
-        self#expr expr2
-        self#bloc li
-    in match Expr.unfix expr2 with
-    | Expr.BinOp (expr3, Expr.Sub, Expr.Fixed.F (_, Expr.Lief (Expr.Integer 1))) ->
-      Format.fprintf f "@[<h>for@ %a@ in@ range(%a,@ %a):@\n@]%a"
-        self#binding varname
-        self#expr expr1
-        self#expr expr3
-        self#bloc li
-    | _ -> default ()
 
   method print_proto f (funname, t, li) =
     Format.fprintf f "def %a(%a):"
@@ -324,30 +395,6 @@ def skipchar():
           self#print_args exprs
       else
         Format.fprintf f "@[<h>print(\"%s\" %% (%a), end='')@]" format self#print_args exprs
-
-  method print f t expr =
-    match Expr.unfix expr with
-    | Expr.Lief (Expr.String s) ->
-      if String.ends_with s "\n" then
-        let l = String.length s in
-        Format.fprintf f "@[print(%S)@]" (String.sub s 0 (l - 1) )
-      else Format.fprintf f "@[print(%S, end='')@]" s
-    | _ ->
-      Format.fprintf f "@[print(\"%a\" %% %a, end='')@]" self#format_type t
-        (fun f expr -> self#exprp prio_percent_percent f expr) expr
-
-  method field f field = Format.fprintf f "%S" field
-
-  method def_fields name f li =
-    print_list
-      (fun f (fieldname, expr) ->
-        Format.fprintf f "@[<h>%a:%a@]"
-          self#field fieldname
-          self#expr expr
-      ) sep_cnl f li
-
-  method allocrecord f name t el =
-    Format.fprintf f "%a = {%a}" self#binding name (self#def_fields name) el
 
   method m_field f m field = Format.fprintf f "%a[%a]" self#mutable_get m self#field field
 end
